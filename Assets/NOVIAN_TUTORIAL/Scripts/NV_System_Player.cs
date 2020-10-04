1b908edafd722094e5e45214abc2a42015c3e2d1
iqbalklv <49069024+iqbalklv@users.noreply.github.com>
2020-10-04 20:30:10 +0700

    bikin noviantutorial

---
+++ Assets/NOVIAN_TUTORIAL/Scripts/NV_System_Player.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NV_System_Player : MonoBehaviour
+{
+    [SerializeField] private float playerSpeed;
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            //MoveUp();
+            this.gameObject.GetComponent<Transform>().Translate(0f, playerSpeed * Time.deltaTime, 0f);
+        }
+    }
+
+    private void MoveUp()
+    {
+        //Akses Titik Y dari Player
+        Transform playerTransform = this.gameObject.GetComponent<Transform>();
+
+        Vector2 playerPosition = playerTransform.position;
+
+        float posisiYLama = playerPosition.y;
+        //----------------------------------
+
+
+        //Naikin Posisi Player Ke Atas
+
+        float posisiYBaru = posisiYLama + (playerSpeed * Time.deltaTime);
+
+        //-----------------------------
+
+
+        //Setor posisi baru ke TRANSFORM
+        playerTransform.position = new Vector2(playerTransform.position.x, posisiYBaru);
+        //
+    }
+}
