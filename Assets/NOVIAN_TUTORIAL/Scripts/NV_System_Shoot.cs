3d93d8de716f32662d153cce135cb7ff897d1f78
iqbalklv <49069024+iqbalklv@users.noreply.github.com>
2020-10-05 17:55:46 +0700

    tembak

---
+++ Assets/NOVIAN_TUTORIAL/Scripts/NV_System_Shoot.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NV_System_Shoot : MonoBehaviour
+{
+    [SerializeField] private GameObject pelor;
+    [SerializeField] private Transform posisiSenjata;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(pelor, posisiSenjata.position, Quaternion.identity);
+        }
+    }
+
+}
