692e225dff120b0d3f3b6707f283688abdb444c2
iqbalklv <49069024+iqbalklv@users.noreply.github.com>
2020-10-09 18:48:48 +0700

    Revert "Lompat"
    This reverts commit d346026a8ad7db5946f33a92f1620f72b9611b20.

--- Assets/NOVIAN_TUTORIAL/Scripts/NV_System_Shoot.cs
+++ Assets/NOVIAN_TUTORIAL/Scripts/NV_System_Shoot.cs
@@ -1,25 +1,18 @@
-﻿ using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class NV_System_Shoot : MonoBehaviour
 {
-    [SerializeField] private GameObject peluru1;
-    [SerializeField] private GameObject peluru2;
-    [SerializeField] private Transform posisiSenjata1;
-    [SerializeField] private Transform posisiSenjata2;
-
-
+    [SerializeField] private GameObject pelor;
+    [SerializeField] private Transform posisiSenjata;
 
     private void Update()
     {
-       
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                Instantiate(peluru1, posisiSenjata1.position, Quaternion.identity);
-                Instantiate(peluru2, posisiSenjata2.position, Quaternion.identity);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(pelor, posisiSenjata.position, Quaternion.identity);
         }
-        
     }
-     
+
 }
